688a3e1bd148d379ec139609f19f579afc3b0760
norbyv1 <127006485+norbyv1@users.noreply.github.com>
2026-02-27 23:57:13 -0800

    Update Tokyo Lib Example to new source URL

--- 1 Tokyo Lib (FIXED)/Tokyo Lib Example.lua
+++ 1 Tokyo Lib (FIXED)/Tokyo Lib Example.lua
@@ -2,7 +2,7 @@ local Decimals = 4
 local Clock = os.clock()
 local ValueText = "Value Is Now :"
 
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/drillygzzly/Roblox-UI-Libs/main/1%20Tokyo%20Lib%20(FIXED)/Tokyo%20Lib%20Source.lua"))({
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/norbyv1/Roblox-UI-Libs/main/1%20Tokyo%20Lib%20(FIXED)/Tokyo%20Lib%20Source.lua"))({
     cheatname = "Title Here", -- watermark text
     gamename = "Title Here", -- watermark text
 })
@@ -174,3 +174,4 @@ library:SendNotification(("Loaded In "..tostring(Time)), 6)
     library:LoadConfig(Default)
     library:SaveConfig(Default)
 ]]
+
